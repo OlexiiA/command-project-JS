2f6450047ccdc6ec4e4500a06b183f2a226f52e1
OlexiiA <incretiblealex@gmail.com>
2022-11-26 23:34:19 +0200

    get-api-to-general-page

--- src/JS/api-server.js
+++ src/JS/api-server.js
@@ -1,12 +1,35 @@
+
 import axios from 'axios';
 
 const KEY = 'api_key=2913964819360854cc0ff757d62600b5'
 
-async function getData(reqest) {
+export async function getData() {
     const apiData = await axios.get(
-      `https://api.themoviedb.org/3/${reqest}?${KEY}`
+      `https://api.themoviedb.org/3/trending/movie/day?${KEY}`
     );
-    return apiData.data.results;
+    return apiData;
   }
-  
-  export default { getData };
+
+
+const divRef = document.querySelector(`.film-card`)
+ 
+ export function addMarkup(data) {
+  const tamplate = data.map(({ title, release_date, poster_path}) => {
+     let releaseYear = release_date.slice(0, 4);
+     return `<li class="card gallery__item">
+        <a href="#" class="card__link">
+          <div class="card__wrapper-img">
+            <img class="card__img" src="https://image.tmdb.org/t/p/w780/${poster_path}" alt="movie's poster">
+          </div>
+          <div class="card__wrapper">
+            <h3 class="card__title">${title}</h3>
+            <p class="card__info"><span class="card__info-genre">${releaseYear}</span></p>
+          </div>
+        </a>
+      </li>`;
+    })
+    .join('');
+  divRef.insertAdjacentHTML('beforeend', tamplate); 
+};
+
+getData().then(res => addMarkup(res.data.results))
--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import API from "./JS/api-server";
-
-//! Пример запроса на популярные 20 фильмов (первая загрузка)
-const popularForDay = 'trending/all/day'
-let proba = API.getData(popularForDay)
-console.log(proba);
+import { getData, addMarkup } from './JS/api-server';
