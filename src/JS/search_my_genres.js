28a27bd29e03915984ef3dd9fd4c3a667547c5ed
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-11-27 00:41:39 +0100

    make something

---
+++ src/JS/search_my_genres.js
@@ -0,0 +1,12 @@
+// import genresData from './genres.json';
+// import axios from 'axios';
+
+// const KEY = 'api_key=2913964819360854cc0ff757d62600b5'
+
+// async function genre() {
+//    const apiData = await axios.get(
+//    `https://api.themoviedb.org/3/genre/movie/list?api_key=2913964819360854cc0ff757d62600b5&language=en-US`
+//    );
+//    return apiData;
+// }
+// console.log(genre());
