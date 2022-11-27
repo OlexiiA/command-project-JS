68c225ef4cdf84df07629d185c0e3c635865ea60
OlexiiA <incretiblealex@gmail.com>
2022-11-28 00:18:10 +0200

    change function

--- src/JS/api-server.js
+++ src/JS/api-server.js
@@ -1,4 +1,26 @@
-import genresArray from './genresArray';
+// import genresArray from './genresArray';
+const genresArray = {
+     28: "Action",
+     12: "Adventure",
+     16: "Animation",
+     35: "Comedy",
+     80: "Crime",
+     99: "Documentary",
+     18: "Drama",
+     10751: "Family",
+     14: "Fantasy",
+     36: "History",
+     27: "Horror",
+     10402: "Music",
+     9648: "Mystery",
+     10749: "Romance",
+     878: "Science Fiction",
+     10770: "TV Movie",
+     53: "Thriller",
+     10752: "War",
+     37: "Western"
+  }
+
 import axios from 'axios';
 
 const KEY = 'api_key=2913964819360854cc0ff757d62600b5'
