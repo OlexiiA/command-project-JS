b121b4a6fb1a0b04f84f597c39a77eeeb671abb5
Асмоловский Андрей <asmolovskiy0202@gmail.com>
2022-11-27 19:57:25 +0100

    working search genres

--- src/JS/choices.js
+++ src/JS/choices.js
@@ -1,10 +1,10 @@
-import Choices from 'choices.js';
-let selectStateInputEl = document.querySelector('.genre-filter');
-const choices = new Choices(selectStateInputEl, {
-  searchEnabled: false,
-  itemSelectText: '',
-});
-let selectGenre = document.querySelector('.genre');
-const choicesgenre = new Choices(selectGenre, {
-  itemSelectText: '',
-});
+// import Choices from 'choices.js';
+// let selectStateInputEl = document.querySelector('.genre-filter');
+// const choices = new Choices(selectStateInputEl, {
+//   searchEnabled: false,
+//   itemSelectText: '',
+// });
+// let selectGenre = document.querySelector('.genre');
+// const choicesgenre = new Choices(selectGenre, {
+//   itemSelectText: '',
+// });
--- src/JS/search_my_genres.js
+++ src/JS/search_my_genres.js
@@ -1,12 +1,58 @@
-// import genresData from './genres.json';
-// import axios from 'axios';
+import genresData from './genres.json';
+import axios from 'axios';
 
-// const KEY = 'api_key=2913964819360854cc0ff757d62600b5'
+const KEY = 'api_key=2913964819360854cc0ff757d62600b5'
 
-// async function genre() {
-//    const apiData = await axios.get(
-//    `https://api.themoviedb.org/3/discover/movie?${KEY}&with_genres=28,12`
-//    );
-//    return apiData;
-// }
-// console.log(genre());
+const searchBtn = document.querySelector('.genres-btn')
+const select = document.querySelector('.genre-filter')
+let genresList = []
+
+
+
+searchBtn.addEventListener('click', showFilms)
+select.addEventListener('change', onSelectChange)
+
+
+async function genre(genresListWithCommas) {
+   const apiData = await axios.get(
+   `https://api.themoviedb.org/3/discover/movie?${KEY}&with_genres=${genresListWithCommas}`
+   );
+   return apiData;
+}
+
+const divRef = document.querySelector(`.film-card`)
+
+function addMarkup(data) {
+const tamplate = data.map(({ title, release_date, poster_path}) => {
+   let releaseYear = release_date.slice(0, 4);
+   return `<li class="card gallery__item">
+      <a href="#" class="card__link">
+         <div class="card__wrapper-img">
+         <img class="card__img" src="https://image.tmdb.org/t/p/w780/${poster_path}" alt="movie's poster">
+         </div>
+         <div class="card__wrapper">
+         <h3 class="card__title">${title}</h3>
+         <p class="card__info"><span class="card__info-genre">${releaseYear}</span></p>
+         </div>
+      </a>
+   </li>`;
+   })
+   .join('');
+divRef.innerHTML = tamplate; 
+};
+
+function showFilms() { //! Рендер разметки по нажатию на кнопку
+   let genresListWithCommas = genresList.map(genre => genre).join(',')
+   genre(genresListWithCommas).then(res => addMarkup(res.data.results))
+   
+   console.log(genresListWithCommas);
+}
+
+function onSelectChange(event) { //! Добавляет в список жанры, если уже есть то удаляет
+   if (genresList.includes(event.target.id)){
+      genresList.splice(genresList.indexOf('event.target.id',1))
+   } else {
+      genresList.push(event.target.id)
+   }
+   console.log(genresList);
+}
