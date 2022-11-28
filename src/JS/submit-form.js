d578ca236f059e136d575de8e4af4e50fcbba0fc
Piligrim41m <lightsun41m@gmail.com>
2022-11-28 22:51:23 +0200

    add fix

--- src/JS/submit-form.js
+++ src/JS/submit-form.js
@@ -4,13 +4,20 @@ export const form = document.querySelector('form#search-form');
 const gallery = document.querySelector('.film-card');
 let searchQuery = '';
 
-const divRef = document.querySelector(`.film-card`)
+export function formListener() {form.addEventListener('submit', onSubmitForm)}
+
 function renderCard(arr) {
     const markup = arr.map(({ poster_path, release_date, title, original_title }) => {
+        let poster = `https://image.tmdb.org/t/p/w780${poster_path}`
+        console.log(poster)
+        if (poster_path === null) {
+            poster = '../images/not-found.png'
+        }
+        console.log(poster)
         let releaseYear = release_date.slice(0, 4);
     return `<div class="film-card">
         <img class="film-photo"
-          src="https://image.tmdb.org/t/p/w780${poster_path}"
+          src=${poster}
           alt="Poster: ${original_title}" />
         <h2 class="film-title"> ${title} </h2>
         <p class="film-year"> ${releaseYear} </p>
@@ -33,12 +40,16 @@ export async function onSubmitForm(event) {
         if (searchResponse.total_results > 0) {
             gallery.innerHTML = ''
             renderCard(searchResponse.results)
+        } else {
+            gallery.innerHTML = ''
+            alert('Sorry, nothing was found for your search.')
         }
     }
     catch (error) {
         console.log(error)
+        
     }
     
 
-    // form.reset()
+    form.reset()
  }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,2 @@
 import { getData, addMarkup } from './JS/api-server';
-import { onSubmitForm, form } from './JS/submit-form';
-form.addEventListener('submit', onSubmitForm)
+import {formListener} from './JS/submit-form'
