086819d58a9a37bd8a3cbb8788e3b134c3a397c6
OlexiiA <106885350+OlexiiA@users.noreply.github.com>
2022-12-05 16:40:58 +0000

    Deploying to gh-pages from @ OlexiiA/command-project-JS@d0eb7088415f691979f6ffe37645da6bd08f7ba5 🚀

---
+++ My-libruary.a90f0734.js
@@ -0,0 +1,2 @@
+!function(){const e=document.querySelector(".footer__button"),t=document.querySelector(".btn-close"),c=document.querySelector(".backdrop-team");e.addEventListener("click",(function(){c.classList.remove("is-hidden")})),t.addEventListener("click",(function(){c.classList.add("is-hidden")}))}();
+//# sourceMappingURL=My-libruary.a90f0734.js.map
---
+++ My-libruary.fe1bc6dd.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".footer__button"),t=document.querySelector(".btn-close"),c=document.querySelector(".backdrop-team");e.addEventListener("click",(function(){c.classList.remove("is-hidden")})),t.addEventListener("click",(function(){c.classList.add("is-hidden")}));
+//# sourceMappingURL=My-libruary.fe1bc6dd.js.map
